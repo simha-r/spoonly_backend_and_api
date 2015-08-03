9e71dca3eb00dfe91df706c9d834f55ccec3a749
L N Reddy <simhaece@gmail.com>
2015-08-03 19:41:05 +0530

    pusher config in initializer only for dev...heroku auto takes care of pusher prod credentials

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,7 @@
 require 'pusher'
-Pusher.app_id = ENV['PUSHER_APP_ID']
-Pusher.key = ENV['PUSHER_KEY']
-Pusher.secret = ENV['PUSHER_SECRET']
+if Rails.env.development?
+  Pusher.app_id = ENV['PUSHER_APP_ID']
+  Pusher.key = ENV['PUSHER_KEY']
+  Pusher.secret = ENV['PUSHER_SECRET']
+end
 Pusher.logger=Rails.logger
