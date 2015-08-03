961313c9c220a48db331fac52cef6f72a91d668d
L N Reddy <simhaece@gmail.com>
2015-08-03 19:33:49 +0530

    Modified pusher config format

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,5 @@
 require 'pusher'
-Pusher.url = ENV['PUSHER_URL']
-Pusher.logger = Rails.logger
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
+Pusher.logger=Rails.logger
