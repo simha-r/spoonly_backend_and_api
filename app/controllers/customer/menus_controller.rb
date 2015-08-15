723e9235bd2848d497d8210f005223750143f150
Goutham Pilla <goutham2027@gmail.com>
2015-08-16 00:20:13 +0530

    - Lunch menu screen

--- app/controllers/customer/menus_controller.rb
+++ app/controllers/customer/menus_controller.rb
@@ -16,7 +16,7 @@ class Customer::MenusController < ApplicationController
   def lunch
     @cart.set_category 'lunch'
     todays_menu = Menu.where(menu_date: Date.today).first
-    if Time.now< todays_menu.lunch_order_end_time
+    if Time.now < todays_menu.lunch_order_end_time
       @menu = todays_menu
     else
       @menu = Menu.where(menu_date: Date.tomorrow).first
