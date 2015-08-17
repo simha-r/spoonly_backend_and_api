00c3d4ad5feb69987ed6bebe21ffac5f8bb07c32
L N Reddy <simhaece@gmail.com>
2015-08-17 13:47:32 +0530

    Fixed Facebook google signin

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::SessionsController < Api::V1::BaseController
   # curl -v -H "Accept: application/vnd.healthy_lunch" -H "Content-type: application/json" -X POST -d '{"provider": "provider", "access_token": "access_token"}'  http://localhost:3000/api/sessions
   def create
     #Find email for that provider,uid,access_token
-    get_user_info params[:access_token],params[:provider]
-    auth = {provider: params[:provider],uid: @uid,token: params[:access_token],info: {email: @email,name: @name}}
+    get_user_info params['access_token'],params['provider']
+    auth = {provider: params['provider'],uid: @uid,token: params['access_token'],info: {email: @email,name: @name}}
 
     user = User.from_omniauth(auth, current_user)
     if user.persisted?
