c434cdc0e204a8d1c0b6b9c98c90c73163696973
L N Reddy <simhaece@gmail.com>
2015-08-17 14:33:15 +0530

    Return error response if user info is not gettable

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,15 +6,18 @@ class Api::V1::SessionsController < Api::V1::BaseController
   # curl -v -H "Accept: application/vnd.healthy_lunch" -H "Content-type: application/json" -X POST -d '{"provider": "provider", "access_token": "access_token"}'  http://localhost:3000/api/sessions
   def create
     #Find email for that provider,uid,access_token
-    get_user_info params['access_token'],params['provider']
-    auth = {provider: params['provider'],uid: @uid,token: params['access_token'],info: {email: @email,name: @name}}
+    if get_user_info params['access_token'],params['provider']
+      auth = {provider: params['provider'],uid: @uid,token: params['access_token'],info: {email: @email,name: @name}}
 
-    user = User.from_omniauth(auth, current_user)
-    if user.persisted?
-      user.confirm!
-      render json: user, status: 200
+      user = User.from_omniauth(auth, current_user)
+      if user.persisted?
+        user.confirm!
+        render json: user, status: 200
+      else
+        render json: { errors: "Authentication failed" }, status: 422
+      end
     else
-      render json: { errors: "Authentication failed" }, status: 422
+      head 422,content_type: 'application/json'
     end
   end
 
