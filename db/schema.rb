9be3df40037a4bb7858e9aa515d069a36b1594fd
L N Reddy <simhaece@gmail.com>
2015-08-17 14:12:30 +0530

    make phone_numnber_verified default to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815143515) do
+ActiveRecord::Schema.define(version: 20150817084107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,7 +135,7 @@ ActiveRecord::Schema.define(version: 20150815143515) do
   create_table "profiles", force: true do |t|
     t.string   "name"
     t.string   "phone_number"
-    t.boolean  "phone_number_verified"
+    t.boolean  "phone_number_verified",          default: false
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
