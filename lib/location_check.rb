4dc668da74871f709fe69fbea0d16368ba69297e
L N Reddy <simhaece@gmail.com>
2015-08-17 23:49:01 +0530

    optimized location check code

--- lib/location_check.rb
+++ lib/location_check.rb
@@ -1,13 +1,13 @@
 module LocationCheck
 
   def check_location
-    values = JSON.parse(cookies[:customer] || {}.to_json).with_indifferent_access
     proceed = false
-    if values[:location]
-      if values[:location][:distance].to_f < 10
-        proceed = true
-      else
-        proceed = false
+    if cookies[:customer]
+      values = JSON.parse(cookies[:customer] || {}.to_json).with_indifferent_access
+      if values[:location]
+        if values[:location][:distance].to_f < 10
+          proceed = true
+        end
       end
     end
     redirect_to main_customer_homes_path unless proceed
