a97fd58674b294c5b24f0ec2803efb4ccbf8df93
L N Reddy <simhaece@gmail.com>
2015-08-19 18:42:40 +0530

    Changed headers accept to spoonly

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,6 @@ class ApiConstraints
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.healthy_lunch.v#{@version}")
+    @default || req.headers['Accept'].include?("application/vnd.spoonly.v#{@version}")
   end
 end
