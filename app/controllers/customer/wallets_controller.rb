0763380928142a3fd0ff25b0a69b7702c76c11ff
L N Reddy <simhaece@gmail.com>
2015-08-19 20:34:48 +0530

    debug info for instamojo

--- app/controllers/customer/wallets_controller.rb
+++ app/controllers/customer/wallets_controller.rb
@@ -7,25 +7,32 @@ before_action :authenticate_user!,except: [:recharge]
   end
 
   def recharge
+    #TODO This url can be refreshed to...so customer may add same money multiple times...store payment_id in wallet
+    # record and then check if already present
     payment_id = params[:payment_id]
     HealthyLunchUtils.log_info "Payment Id is #{payment_id}"
+    HealthyLunchUtils.log_info "Params are #{params.inspect}"
 
     url = "https://www.instamojo.com/api/1.1/payments/"+payment_id+"/"
     response = Typhoeus.get(url,headers: {'X-Auth-Token'=>ENV['INSTAMOJO_AUTH_TOKEN'],'Accept'=>'application/vnd.spoonly.v1','X-Api-Key'=>ENV['INSTAMOJO_API_KEY']})
+    HealthyLunchUtils.log_info "Response code is #{response.code}"
+    HealthyLunchUtils.log_info "Response  is #{response.inspect}"
 
     if response.code==200
       response = JSON.parse(response.response_body)
+      HealthyLunchUtils.log_info "JSON Response  is #{response.inspect}"
       if response['success']==true
         amount = response['payment']['amount']
         user_id = response['payment']['user_id']
         @user = User.find user_id
         if @user.add_to_wallet amount
-          redirect_to successful_recharge_customer_wallet_path
+         return redirect_to successful_recharge_customer_wallet_path
         else
-          redirect_to failed_recharge_customer_wallet_path
+          return redirect_to failed_recharge_customer_wallet_path
         end
       end
     end
+    redirect_to failed_recharge_customer_wallet_path
   end
 
 
