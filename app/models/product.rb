3fdb354ef4e00105fcadb165641f9a618a9611d8
L N Reddy <simhaece@gmail.com>
2015-08-22 10:53:39 +0530

    aded paperclip optimizer

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,7 @@ class Product < ActiveRecord::Base
 
   has_attached_file :photo,:convert_options => { :mobile => "-strip -quality 50" },
                     :styles => { :medium => "300x300", :mobile => "1440*960",:thumb=>"100*100" },
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "/images/:style/missing.png",processors: [:thumbnail, :paperclip_optimizer]
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 
