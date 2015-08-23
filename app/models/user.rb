1d3ec84243d97521ee9843c47622453c1a43d4aa
L N Reddy <simhaece@gmail.com>
2015-08-23 12:53:09 +0530

    Removed registerable module for users,company users

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable and :
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable,:omniauthable
 
   delegate :name,:phone_number,:phone_number_verified, to: :profile
