caa6673f77e111ab92eae40ffee67201dff03c8b
L N Reddy <simhaece@gmail.com>
2015-08-24 16:53:05 +0530

    send order success message

--- lib/user_messenger.rb
+++ lib/user_messenger.rb
@@ -3,8 +3,15 @@ module UserMessenger
 
   def self.order_success order
     user =order.user
-    message = "Dear #{user.name}, Thank you for using Spoonly. Your order ##{order.id} will be delivered by #{order
-    .delivery_time_range}."
+    delivery_date = order.delivery_time.to_date
+    if delivery_date != Date.today
+      message = "Dear #{user.name}, Thank you for using Spoonly. Your order ##{order.id} will be delivered on #{order
+      .delivery_time.to_date.strftime("%a,%-d %b")} by #{order.delivery_time_range}."
+    else
+      message = "Dear #{user.name}, Thank you for using Spoonly. Your order ##{order.id} will be delivered by #{order
+      .delivery_time_range}."
+    end
+
     if order.cash_to_pay>0
       message = message+"Please pay Rs #{order.cash_to_pay} to the delivery executive."
     end
