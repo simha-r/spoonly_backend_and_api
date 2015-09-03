94a52e16ac28a8d5b4adb5e87d636a98f27b41de
L N Reddy <simhaece@gmail.com>
2015-09-03 11:00:29 +0530

    Send share screen text api

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::PromotionsController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def referral
-    render json: {show_text: "Share your promo code with a friend. Your friend gets Rs 50, you get Rs 50 when they order!", share_text:'get Rs 50 in your wallet. Download the app - http://spoonly.in'}
+    referral_code = current_user.referral_code
+    share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app - http://spoonly.in"
+    render json: {show_text: "Share your promo code with a friend. Your friend gets Rs 50, you get Rs 50 when they order!", share_text: share_text, referral_code: referral_code}
   end
 
 end
