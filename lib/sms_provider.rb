601be774e8905f602bd6606bc7bd8fb235e62f54
L N Reddy <simhaece@gmail.com>
2015-09-09 15:17:37 +0530

    Switched sms provider to Infini solutions

--- lib/sms_provider.rb
+++ lib/sms_provider.rb
@@ -2,9 +2,24 @@ require 'nexmo'
 
 module SmsProvider
 
-  def self.send_message phone_number,message
+  def send_message_with_nexmo phone_number,message
     nexmo = Nexmo::Client.new(key: ENV['NEXMO_API_KEY'], secret: ENV['NEXMO_API_SECRET'])
     nexmo.send_message(from: 'SPOONL', to: "91#{phone_number}", text: message)
+  end
+
+  def self.send_message_with_infini phone_number,message
+    response = HTTParty.post("http://alerts.solutionsinfini.com/api/v3/index.php",
+                  {
+                    :query => { "method" => "sms", "api_key" => ENV["INFINI_SMS_API_KEY"],
+                                 "to" => "#{phone_number}","sender"=>"SPOONL","message"=>message,
+                                 "format"=>"json" }
+                  })
+    # http://URL/api/v3/index.php?method=sms&api_key=Ad9e5XXXXXXXXXXXXX&to=997XXXXXXX,997XXXXXXX&sender=INFXXX&message=testing&
+    #   format=json&custom=1,2&flash=0
+  end
+
+  def self.send_message phone_number,message
+    self.send_message_with_infini phone_number,message
   end
 
 end
