2e09abeda3c28bf0f8352b837fcc29ceeb57e5de
L N Reddy <simhaece@gmail.com>
2015-09-18 15:53:22 +0530

    minor copy changes

--- app/controllers/customer/omniauth_callbacks_controller.rb
+++ app/controllers/customer/omniauth_callbacks_controller.rb
@@ -8,7 +8,6 @@ class Customer::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
     session[:login_type]=user.login_type
     if user.persisted?
       user.confirm!
-      flash[:notice] = "You are in..!!! Go to edit profile to see the status for the accounts"
       sign_in_and_redirect(user)
     else
       session["devise.user_attributes"] = user.attributes
