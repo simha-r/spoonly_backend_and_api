b3466f1fe0d0a19ed668a316a57a6b9255291a11
L N Reddy <simhaece@gmail.com>
2015-09-25 13:04:32 +0530

    changed share text

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::PromotionsController < Api::V1::BaseController
 
   def referral
     referral_code = current_user.referral_code
-    share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app - http://bit.ly/spoonlyapp"
-    render json: {show_text: "Share your promo code with a friend. Your friend gets Rs 50, you get Rs 50 when they order!", share_text: share_text, referral_code: referral_code}
+    share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app - https://play.google.com/apps/testing/com.freshspoon.spoonly"
+    render json: {show_text: "Help us alpha test the app. Share your promo code with a friend. Your friend gets Rs 50,
+you get Rs 50 when they order!", share_text: share_text, referral_code: referral_code}
   end
 
 end
