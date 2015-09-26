a1602d4a760c493dcf5f10b4a8d4f73ef19bdf0b
L N Reddy <simhaece@gmail.com>
2015-09-26 20:50:52 +0530

    fix bug in resend

--- lib/number_verification.rb
+++ lib/number_verification.rb
@@ -9,8 +9,7 @@ module NumberVerification
 
   def self.resend user
     last_generated_time = user.profile.number_verification_code_generated_at
-    elapsed_time =  last_generated ? (Time.now - user.profile
-    .number_verification_code_generated_at) : nil
+    elapsed_time =  last_generated_time ? (Time.now - last_generated_time) : nil
     if(elapsed_time && (elapsed_time > 10.minutes))
       code = Random.rand(10000..99999).to_s
       user.profile.update_attributes(phone_number_verification_code: code,number_verification_code_generated_at: Time.now)
