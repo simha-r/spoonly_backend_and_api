1cf5f3a2a1e2de48354582ae2ea7c5c3d119bf16
L N Reddy <simhaece@gmail.com>
2015-09-28 01:36:49 +0530

    change referral message, also accept referral codes with RR only

--- app/controllers/api/v1/referrals_controller.rb
+++ app/controllers/api/v1/referrals_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::ReferralsController < Api::V1::BaseController
 
   def create
-    referrer = User.where(referral_code: params[:referral_code]).first
-    if current_user.referred || !referrer || current_user.orders.delivered.present? || (current_user.referral_code==params[:referral_code])
-       render json: {error: 'Invalid code'},status: 500
-      #TODO Send error message
-    else
-      if referrer.refer_user current_user
-        render json: {text: 'Promotion has been applied!'}
+    referral_code = params[:referral_code]
+
+    if referral_code[0..1].upcase=='RR'
+      #It is a referral code
+      referrer = User.where(referral_code: params[:referral_code]).first
+      if current_user.referred || !referrer || current_user.orders.delivered.present? || (current_user.referral_code==params[:referral_code])
+         render json: {error: 'Invalid code'},status: 500
+        #TODO Send error message
+      else
+        if referrer.refer_user current_user
+          render json: {text: 'Promotion has been applied!'}
+        end
       end
+    else
+      #Not a referral code
+      #TODO use this for applying general promo codes later
+      render json: {error: 'Invalid code'},status: 500
     end
   end
 
