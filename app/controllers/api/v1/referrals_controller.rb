27d66bca2cdad8443554b9bc839d3148603c276e
L N Reddy <simhaece@gmail.com>
2015-09-27 23:05:34 +0530

    prevent users from applying their own referral code

--- app/controllers/api/v1/referrals_controller.rb
+++ app/controllers/api/v1/referrals_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::ReferralsController < Api::V1::BaseController
 
   def create
     referrer = User.where(referral_code: params[:referral_code]).first
-    if current_user.referred || !referrer || current_user.orders.delivered.present?
+    if current_user.referred || !referrer || current_user.orders.delivered.present? || (current_user.referral_code==params[:referral_code])
        render json: {error: 'Invalid code'},status: 500
       #TODO Send error message
     else
       if referrer.refer_user current_user
-        render json: {text: 'Promotion has been applied'}
+        render json: {text: 'Promotion has been applied!'}
       end
     end
   end
