872d8cf92886dda57530d28c3c744f36ab087e13
L N Reddy <simhaece@gmail.com>
2015-09-29 00:24:31 +0530

    added iiit to serving area

--- config/initializers/location.rb
+++ config/initializers/location.rb
@@ -1,5 +1,5 @@
 #TODO Storing the border_patrol serving area in application config..so that we dont need to parse the file on each
 # request
-file = File.read('data/mindspace_dlf_synergy_bc_meenakshi.kml')
+file = File.read('data/mindspace_synergy_dlf_iiit_meenakshi.kml')
 Rails.application.config.serving_region = BorderPatrol.parse_kml(file)
 puts "Initialized serving region"
