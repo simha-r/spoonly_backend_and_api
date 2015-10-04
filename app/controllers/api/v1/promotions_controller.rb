6acb64db6427db1177b64fa7b03b0fba96547c25
L N Reddy <simhaece@gmail.com>
2015-10-05 00:55:15 +0530

    changed referral code logo

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::PromotionsController < Api::V1::BaseController
 
   def referral
     referral_code = current_user.referral_code
-    referral_link = "http://www.spoonly.in/app"
-    # referral_link = "http://www.spoonly.in/gifts/#{referral_code}"
+    # referral_link = "http://www.spoonly.in/app"
+    referral_link = "http://www.spoonly.in/gifts/#{referral_code}"
     share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app at #{referral_link}"
     render json: {show_text: "Know anyone in or near Mindspace,CtrlS or TCS Synergy Park ?
 Share your promo code with them.
