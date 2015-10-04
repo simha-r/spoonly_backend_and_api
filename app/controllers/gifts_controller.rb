b50f21adef7f694bd788531757d2649b17bd5340
Goutham Pilla <goutham2027@gmail.com>
2015-10-04 23:57:56 +0530

    - Referral page

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,8 +1,14 @@
 class GiftsController < Customer::BaseController
 
+  layout :layout_by_resource
+
   def show
     referral_code = params[:id]
     @referrer = User.where(referral_code: referral_code).first
+  end
+
+  def layout_by_resource
+    "gifts"
   end
 
 
