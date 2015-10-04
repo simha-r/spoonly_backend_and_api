30c29d0fee92ce373a21b830e265524175de2044
L N Reddy <simhaece@gmail.com>
2015-10-05 00:55:57 +0530

    change share text

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -7,9 +7,8 @@ class Api::V1::PromotionsController < Api::V1::BaseController
     # referral_link = "http://www.spoonly.in/app"
     referral_link = "http://www.spoonly.in/gifts/#{referral_code}"
     share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app at #{referral_link}"
-    render json: {show_text: "Know anyone in or near Mindspace,CtrlS or TCS Synergy Park ?
-Share your promo code with them.
-Your friend gets Rs 50,you get Rs 50 after they order!", share_text: share_text,referral_code: referral_code}
+    render json: {show_text: "We currently serve in parts of Hitech City & Gachibowli. Share Spoonly with a
+friend and get Rs 50 when they order.", share_text: share_text,referral_code: referral_code}
   end
 
 end
