ff653444fad81bfed739cd530a2b0774d9910093
L N Reddy <simhaece@gmail.com>
2015-10-06 00:25:44 +0530

    changed promotion line

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -7,8 +7,7 @@ class Api::V1::PromotionsController < Api::V1::BaseController
     # referral_link = "http://www.spoonly.in/app"
     referral_link = "http://www.spoonly.in/gifts/#{referral_code}"
     share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app at #{referral_link}"
-    render json: {show_text: "We currently serve in parts of Hitech City & Gachibowli. Share Spoonly with a
-friend and get Rs 50 when they order.", share_text: share_text,referral_code: referral_code}
+    render json: {show_text: "Share Spoonly with a friend and get Rs 50 when they order.", share_text: share_text,referral_code: referral_code}
   end
 
 end
