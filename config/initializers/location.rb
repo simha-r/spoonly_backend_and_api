18528cc166983346d74a663eb139082232811b7c
L N Reddy <simhaece@gmail.com>
2015-10-06 20:39:59 +0530

    increased serving area

--- config/initializers/location.rb
+++ config/initializers/location.rb
@@ -1,5 +1,5 @@
 #TODO Storing the border_patrol serving area in application config..so that we dont need to parse the file on each
 # request
-file = File.read('data/serving_area_5_oct.kml')
+file = File.read('data/serving_area_oct_7th.kml')
 Rails.application.config.serving_region = BorderPatrol.parse_kml(file)
 puts "Initialized serving region"
