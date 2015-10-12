ae911cfb7a61031cb224eb083513c9f1a6a0751e
L N Reddy <simhaece@gmail.com>
2015-10-12 22:42:06 +0530

    Change referral message

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -7,7 +7,8 @@ class Api::V1::PromotionsController < Api::V1::BaseController
     # referral_link = "http://www.spoonly.in/app"
     referral_link = "http://www.spoonly.in/gifts/#{referral_code}"
     share_text = "Use my Spoonly Promo Code #{referral_code} and get Rs 50 in your wallet. Download the app at #{referral_link}"
-    render json: {show_text: "Share Spoonly with a friend and get Rs 50 when they order.", share_text: share_text,referral_code: referral_code}
+    render json: {show_text: "Good food must be shared! Gift friends a free meal worth &#8377 50 and earn &#8377 50
+after they order",share_text: share_text,referral_code: referral_code}
   end
 
 end
