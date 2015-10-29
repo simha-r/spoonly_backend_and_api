cc114905282e9c8c8e8ee795b7fbeca216e96b62
L N Reddy <simhaece@gmail.com>
2015-10-29 10:42:42 +0530

    users search

--- app/controllers/company/users_controller.rb
+++ app/controllers/company/users_controller.rb
@@ -1,10 +1,16 @@
 class Company::UsersController < Company::BaseController
 
   before_filter :authenticate_admin!
-  before_filter :load_resource, except: [:index]
+  before_filter :load_resource, except: [:index,:search]
 
   def index
-    @users = User.order(created_at: :desc).includes(:profile,:referrals,:wallet).paginate page: params[:page]
+    @search_term = params[:search_term]
+    if @search_term.present?
+      wildcard_search = "%#{@search_term}%"
+      @users = User.where("email iLIKE ?", wildcard_search).includes(:profile,:referrals,:wallet).paginate page: params[:page]
+    else
+      @users = User.order(created_at: :desc).includes(:profile,:referrals,:wallet).paginate page: params[:page]
+    end
   end
 
   def show
@@ -13,6 +19,11 @@ class Company::UsersController < Company::BaseController
 
   def wallet
     
+  end
+
+  def search
+
+    render 'index'
   end
 
   private
