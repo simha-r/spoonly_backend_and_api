1d6d2ae46172fb43a592c2fd306078c8f51d2f98
L N Reddy <simhaece@gmail.com>
2015-11-18 20:34:25 +0530

    show dinner coming soon

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::MenusController < Api::V1::BaseController
     current_user.log_location params['lat'],params['long']
 
     if LocationCheck.in_range? params['lat'],params['long']
-      render json: {'lunch'=> {time: '12:15 - 3:30 PM',url: 'menu/lunch'},show_promo: true}
+      render json: {'lunch'=> {time: '12:15 - 3:30 PM',url: 'menu/lunch'},show_promo: true,'dinner'=> {time: '7:00 - 10:30 PM',url: 'menu/dinner'}}
     else
       render json: {service_unavailable: ENV['OUT_OF_COVERAGE']}
     #TODO Send html to show on client...like We currently dont serve in your area. We serve in selected parts of
@@ -19,7 +19,7 @@ class Api::V1::MenusController < Api::V1::BaseController
 
   #curl -v -H "Accept: application/vnd.healthy_lunch" -H "Content-type: application/application/x-www-form-urlencoded" -H
   # "Authorization:authentication_token"  http://localhost:3000/api/products/lunch
-  def lunch
+  def lunch 
     @menu = Menu.current_lunch
     if @menu
       render json: @menu.show_lunch
@@ -35,12 +35,11 @@ class Api::V1::MenusController < Api::V1::BaseController
 
   def dinner
     @menu = Menu.current_dinner
-    if @menu
+    if @menu.menu_dinner_products.present?
       render json: @menu.show_dinner
     else
-      render json: {notice: 'We are closed today. Please check again tomorrow !'}
+      render json: {notice: 'Dinner is coming soon!'}
     end
   end
-
 
 end
