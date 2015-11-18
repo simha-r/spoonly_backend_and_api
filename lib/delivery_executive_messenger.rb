0228964c64db907230a1d3de1d608906964429b4
L N Reddy <simhaece@gmail.com>
2015-11-18 10:53:49 +0530

    alarm on normal dispatch also

--- lib/delivery_executive_messenger.rb
+++ lib/delivery_executive_messenger.rb
@@ -11,6 +11,9 @@ module DeliveryExecutiveMessenger
 Rs #{order.cash_to_pay.to_i}"
     HealthyLunchUtils.log_info "Sending message to #{delivery_executive.name}"
     SmsProvider.send_message_with_telerivet delivery_executive.phone_number,message
+    Pusher['orders'].trigger("assigned_to_#{delivery_executive.id}", {
+      message: 'New Order Created..Refresh your browser to see it'
+    })
   end
 
   def self.inform_on_field order
