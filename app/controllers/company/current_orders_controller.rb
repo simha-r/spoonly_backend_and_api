c2ad219eabe9b5072a4e29544408715d43f60c77
L N Reddy <simhaece@gmail.com>
2015-11-24 09:42:57 +0530

    added date range for heatmap

--- app/controllers/company/current_orders_controller.rb
+++ app/controllers/company/current_orders_controller.rb
@@ -52,7 +52,17 @@ class Company::CurrentOrdersController < Company::BaseController
   end
 
   def heatmap
-    @user_locations = UserLocation.where("created_at >= ?", Time.zone.now.beginning_of_day).to_a.uniq { |ul| ul.user_id }
+    if params["start_date"].present?
+
+      @start_date_1 = Date.new params['start_date']["start_date(1i)"].to_i, params['start_date']["start_date(2i)"].to_i,
+                                   params['start_date']["start_date(3i)"].to_i
+      @end_date_1 = Date.new params['end_date']["end_date(1i)"].to_i, params['end_date']["end_date(2i)"].to_i,
+                          params['end_date']["end_date(3i)"].to_i
+    else
+      @start_date_1 =  Time.now.beginning_of_day
+      @end_date_1 =  Time.now.end_of_day
+    end
+    @user_locations = UserLocation.where("created_at >= ?  and created_at <= ?", @start_date_1,@end_date_1).to_a.uniq { |ul| ul.user_id }
     @user_locations = @user_locations.collect do |ul|
       [ul.location.latitude.to_f,ul.location.longitude.to_f]
     end
