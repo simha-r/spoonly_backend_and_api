bc77e0c114a2d48557f8fd015094b3eb8f41bc42
L N Reddy <simhaece@gmail.com>
2015-11-24 09:53:10 +0530

    heatmap modify to weight for people using it every day

--- app/controllers/company/current_orders_controller.rb
+++ app/controllers/company/current_orders_controller.rb
@@ -59,10 +59,15 @@ class Company::CurrentOrdersController < Company::BaseController
       @end_date_1 = Date.new params['end_date']["end_date(1i)"].to_i, params['end_date']["end_date(2i)"].to_i,
                           params['end_date']["end_date(3i)"].to_i
     else
-      @start_date_1 =  Time.now.beginning_of_day
-      @end_date_1 =  Time.now.end_of_day
+      @start_date_1 =  Time.zone.now.to_date
+      @end_date_1 =  Time.zone.now.to_date
     end
-    @user_locations = UserLocation.where("created_at >= ?  and created_at <= ?", @start_date_1,@end_date_1).to_a.uniq { |ul| ul.user_id }
+
+    @user_locations = []
+    (@start_date_1..@end_date_1).each do |date|
+      @user_locations << UserLocation.where("date(created_at) = ?",date).to_a.uniq { |ul| ul.user_id }
+    end
+    @user_locations.flatten!
     @user_locations = @user_locations.collect do |ul|
       [ul.location.latitude.to_f,ul.location.longitude.to_f]
     end
