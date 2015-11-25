99551716c6f977a346f3332476c64612dd5c91ec
L N Reddy <simhaece@gmail.com>
2015-11-25 08:28:49 +0530

    show promo even in out of service

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -11,12 +11,12 @@ class Api::V1::MenusController < Api::V1::BaseController
       show_promo = true
       current_user.update_attributes(promo_code_shown: true)
     end
-    current_user.log_location params['lat'],params['long']
+
 
     if LocationCheck.in_range? params['lat'],params['long']
       render json: {'lunch'=> {time: '12:15 - 3:30 PM',url: 'menu/lunch'},show_promo: show_promo,'dinner'=> {time: '7:00 - 10:30 PM',url: 'menu/dinner'}}
     else
-      render json: {service_unavailable: ENV['OUT_OF_COVERAGE']}
+      render json: {service_unavailable: ENV['OUT_OF_COVERAGE'],show_promo: show_promo}
     #TODO Send html to show on client...like We currently dont serve in your area. We serve in selected parts of
     # Hitech City and gachibowli
     end
