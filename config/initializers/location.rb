9cb30275b5e13c55704166f0af3cc88225778f00
L N Reddy <simhaece@gmail.com>
2015-12-08 01:25:30 +0530

    added madhapur and parts of kondapur to serving area

--- config/initializers/location.rb
+++ config/initializers/location.rb
@@ -1,5 +1,5 @@
 #Storing the border_patrol serving area in application config..so that we dont need to parse the file on each
 # request
-file = File.read('data/serving_area_nov_25.kml')
+file = File.read('data/serving_area_dec_8th.kml')
 Rails.application.config.serving_region = BorderPatrol.parse_kml(file)
 puts "Initialized serving region"
