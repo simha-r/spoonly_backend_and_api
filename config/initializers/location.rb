6e193fcb3c7a845f1126ff13e9ad47a3cb144dff
L N Reddy <simhaece@gmail.com>
2016-01-15 11:07:32 +0530

    added some more area rto coverage

--- config/initializers/location.rb
+++ config/initializers/location.rb
@@ -1,7 +1,7 @@
 #Storing the border_patrol serving area in application config..so that we dont need to parse the file on each
 # request
-file = File.read('data/serving_area_dinner_jan_14th.kml')
-dinner_file = File.read('data/serving_area_dinner_jan_14th.kml')
+file = File.read('data/serving_area_jan_15th.kml')
+dinner_file = File.read('data/serving_area_jan_15th.kml')
 Rails.application.config.serving_region = BorderPatrol.parse_kml(file)
 Rails.application.config.lunch_serving_region = BorderPatrol.parse_kml(file)
 Rails.application.config.dinner_serving_region = BorderPatrol.parse_kml(dinner_file)
