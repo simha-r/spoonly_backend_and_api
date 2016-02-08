cdd0326bb9a16b840d2af5bb61e766ca202131f3
L N Reddy <simhaece@gmail.com>
2016-02-08 21:29:49 +0530

    fixed bug in ask feedback controller

--- app/controllers/company/orders_controller.rb
+++ app/controllers/company/orders_controller.rb
@@ -115,8 +115,8 @@ class Company::OrdersController < Company::BaseController
 
   def ask_feedback
     if @order.delivered?
-      @order.mark_feedback_asked
       @order.ask_for_feedback
+      @order.mark_feedback_asked
       redirect_to request.referrer, notice: 'Asked for feedback'
     else
       redirect_to request.referrer, alert: 'Order hasnt been delivered yet'
