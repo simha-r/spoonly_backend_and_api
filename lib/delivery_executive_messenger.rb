1af021f9b033961bae615d126281bb0d4d33a3a7
L N Reddy <simhaece@gmail.com>
2016-02-12 21:17:57 +0530

    send verification code with infini

--- lib/delivery_executive_messenger.rb
+++ lib/delivery_executive_messenger.rb
@@ -10,7 +10,7 @@ module DeliveryExecutiveMessenger
 #{order.brief_line_items}
 Rs #{order.cash_to_pay.to_i}"
     HealthyLunchUtils.log_info "Sending message to #{delivery_executive.name}"
-    SmsProvider.send_message_with_telerivet delivery_executive.phone_number,message
+    # SmsProvider.send_message_with_telerivet delivery_executive.phone_number,message
     Pusher['orders'].trigger("assigned_to_#{delivery_executive.id}", {
       message: 'New Order Created..Refresh your browser to see it'
     })
