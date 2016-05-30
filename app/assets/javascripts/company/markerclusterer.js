592f860729f9590c1dc22a34fe0fef88674adcf7
L N Reddy <simhaece@gmail.com>
2016-05-30 19:18:35 +0530

    fix marker clusterer images in production

--- app/assets/javascripts/company/markerclusterer.js
+++ app/assets/javascripts/company/markerclusterer.js
@@ -187,7 +187,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../../assets/m';
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = 'https://s3-ap-southeast-1.amazonaws.com/healthylunchproduction/markerclusterimages/m';
 
 
 /**
